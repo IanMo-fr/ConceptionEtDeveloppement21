fed1cd539e5e1e4d6ead07f37b32c464f07c8e7e
IanMo-fr <67973878+IanMo-fr@users.noreply.github.com>
2021-03-28 15:22:02 +0200

    création de fichier vide

--- src/sauvegarde/EnregistrementFichier.java
+++ src/sauvegarde/EnregistrementFichier.java
@@ -1,7 +1,43 @@
 package sauvegarde;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbookFactory;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+
 /**
  * Class responsable de sauvegarder les fichiers traitées.
  */
 public class EnregistrementFichier {
+    /**
+     * un <code>HSSFWorkbook</code> qui récupère les données traitées
+     */
+    private HSSFWorkbook xlsAnonymise;
+
+    /**
+     * permet de sauvegarder le chemin d'accès du fichier de sortis
+     * nb : changement du pathName par Constructeur de la class
+     */
+    private String pathSauvegardeFichier;
+
+
+    /**
+     * Constructeur par défaut
+     */
+    public EnregistrementFichier() {
+        this.xlsAnonymise = null;
+        this.pathSauvegardeFichier = "./out/production";
+    }
+
+    /**
+     * Methode permettant de créer un ficher <code>.xls</code> vide
+     * @return vide     Un fichier <code>.xls</code> vide
+     */
+    private HSSFWorkbook Initialisation() {
+        HSSFWorkbookFactory crea = new HSSFWorkbookFactory();
+
+        return crea.create();
+    }
+
 }
