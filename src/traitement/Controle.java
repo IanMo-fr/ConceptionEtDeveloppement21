4c53035e4704fea1135e4005ec58cb88129674ac
IanMo-fr <67973878+IanMo-fr@users.noreply.github.com>
2021-03-28 13:55:24 +0200

    commentaires de la class interaction

--- src/traitement/Controle.java
+++ src/traitement/Controle.java
@@ -4,4 +4,11 @@ package traitement;
  * Class regroupant les differentes sous-classes de traitement de données
  */
 public class Controle {
+
+    /**
+     * Class permettant la pseudomisation
+     */
+    private class Pseudonymisation {
+
+    }
 }
