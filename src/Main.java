8d6279356db96632848f0681ba6e7cb620856fc1
Oger Julien <julien.oger@etu.univ-orleans.fr>
2021-03-30 00:40:30 +0200

    Tentative de mise à jour de la librairie

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ import traitement.Controle;
 public class Main {
     public static void main(String[] args) throws IOException {
         LectureFichier test = new LectureFichier();
-        test.OuvrirFichier("D:\\Documents\\CDA_projet/exemple_ce.xls");
+        test.OuvrirFichier("C:\\Users\\jujuo\\Desktop\\CDA_projet\\exemple_ce.xls");
         test.LireIdentifiants();
         List<List<String>> testeur = test.getListeIdentifiants();
         Controle.Pseudonymisation letest = new Controle.Pseudonymisation();
