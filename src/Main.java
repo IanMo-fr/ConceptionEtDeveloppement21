763221fd8005a0f616bd9c330d24d78eac764702
IanMo-fr <67973878+IanMo-fr@users.noreply.github.com>
2021-04-25 00:55:38 +0200

    retour valeur de base

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ import java.io.IOException;
  * Main du projet : réalise les intéractions utilisateur et fait l'appel <code>Controleur</code>*/
 public class Main {
     public static void main(String[] args) throws IOException {
-       Controleur controleur = new Controleur("Ian", true); //Préciser ici en paramètre le chemin d'accès d'arrivée du fichier, sinon il ya un chemin par défaut
+       Controleur controleur = new Controleur("Julien", true); //Préciser ici en paramètre le chemin d'accès d'arrivée du fichier, sinon il ya un chemin par défaut
        // controleur.CreerDocPseudonymisé("C:/Users/jujuo/Desktop/CDA_proj/exemple_ce.xls");
 
      //  controleur.CreerDocsBucketisés("C:/Users/jujuo/Desktop/CDA_proj/pseudos.xls", 3); //Attention cette commande prend une base de données PSEUDONYMISEE en paramètre
-        controleur.controleurAlgo1("E:/Documents/Etude/L2/Semestre 4/Conception et dev/projetanon/exemple_ce.xls");
+        //controleur.controleurAlgo1("E:/Documents/Etude/L2/Semestre 4/Conception et dev/projetanon/exemple_ce.xls");
 
      controleur.controleurIHM();
       //controleur.VerifierDiversité("C:/Users/jujuo/Desktop/CDA_proj/exemple_ce.xls", 3,3);
