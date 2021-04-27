a7167e2d92273e9e0792a4fced926bc9358a958d
IanMo-fr <67973878+IanMo-fr@users.noreply.github.com>
2021-04-27 02:05:39 +0200

    début de changement d'approche par les string

--- src/traitement/AlgoUnidimensionnel.java
+++ src/traitement/AlgoUnidimensionnel.java
@@ -18,8 +18,8 @@ public class AlgoUnidimensionnel implements ArbreGeneralisation {
                 colonne = i;
         }
 
-        for (int i= 1; i < val_string.size(); i++){
-            sheet_donnees.getRow(i).getCell(colonne).setCellValue(val_string.get(i));
+        for (int i= 0; i < val_string.size(); i++){
+            sheet_donnees.getRow(i+1).getCell(colonne).setCellValue(val_string.get(i));
         }
 
         return wb;
@@ -27,7 +27,11 @@ public class AlgoUnidimensionnel implements ArbreGeneralisation {
 
     public HSSFWorkbook algoUni(List<List<String>> listeQID, String nomAttr, HSSFWorkbook wb){
         List<Integer> liste = selectQIDAtt(listeQID, nomAttr);
-        List<String> aprox =  anonyme(liste);
+        List<String> val_string = new ArrayList<>();
+        for (int i=0; i<liste.size();i++) {
+            val_string.add("");
+        }
+        List<String> aprox =  anonyme(liste,val_string);
         return creerAnonyme(wb, aprox, nomAttr);
     }
 
@@ -63,8 +67,7 @@ public class AlgoUnidimensionnel implements ArbreGeneralisation {
      * @return le Workbook après passage par l'algorithme
      */
     @Override
-    public List<String> anonyme(List<Integer> listeQID) {
-List<Integer> oskour = listeQID;
+    public List<String> anonyme(List<Integer> listeQID, List<String> val_String) {
         //appel de fct mediane sur l'attribut
         int mediane = this.mediane(listeQID);
 
@@ -79,53 +82,53 @@ List<Integer> oskour = listeQID;
         Collections.sort(listeQID);
 
         //on coupe en deux la liste de l'attibut pour avoir une généralisation par la médiane
-        List<Integer> rHands = new ArrayList<>();
-        List<Integer> lHands = new ArrayList<>();
+        String rHands;
+        String lHands;
 
         //Si la médiane est une valeur de liste initialement ou non
-        /*
+
         if (listeQID.contains(mediane)) {
-            lHands = listeQID.subList(0, listeQID.lastIndexOf(mediane)+1);
-            rHands = listeQID.subList(listeQID.indexOf(mediane)+1, listeQID.size());
-            upperMediane++;
+            lHands = listeQID.get(0) + "-" + listeQID.get(listeQID.lastIndexOf(mediane));
+            rHands = (listeQID.indexOf(mediane)+1) +"-" + listeQID.get(listeQID.size()-1);
+            //upperMediane++;
         }else {
 
-         */
             //si jamais la médiane ne se trouve pas dans la liste, on cherche la borne la plus proche de la valeur de la médiane
                 while (!listeQID.contains(lowMediane)) {
                     lowMediane--;
                 }
-                lHands = listeQID.subList(0, listeQID.lastIndexOf(lowMediane) + 1);
+                lHands = listeQID.get(0) + "-" + listeQID.get(listeQID.lastIndexOf(lowMediane));
 
             //on mets un +1 car subList prends la borne sup exclue
 
                 while (!listeQID.contains(upperMediane)) {
                     upperMediane++;
                 }
-                rHands = listeQID.subList(listeQID.indexOf(upperMediane), listeQID.size());
+            rHands = (listeQID.indexOf(mediane)+1) +"-" + listeQID.get(listeQID.size());
+        }
 
-       // }
-        //liste de stockage des valeurs aprox
-        List<String> val_String = new ArrayList<String>();
         //on verifie si la valeur à la cellule visitée est égale à une valeur de l'une des sub-listes
         //et on modifie par la borne basse et haute de la sub-liste correspondante
         for (int i = 0; i <attributOG.size(); i++) {
-
-
             //on rentre l'approximation dans la liste
-            if (lHands.contains(attributOG.get(i)))
-                val_String.add(lHands.get(0) + "-" + lHands.get(lHands.size()-1));
-            else
-                val_String.add(rHands.get(0) + "-" + rHands.get(rHands.size()-1));
+            if ((attributOG.get(i) >= Integer.parseInt(lHands.substring(0,lHands.indexOf("-"))))
+                 && (attributOG.get(i) >= Integer.parseInt(lHands.substring(lHands.indexOf("-")+1,lHands.length())))       )
+                val_String.set(i, lHands);
+            else if ((attributOG.get(i) >= Integer.parseInt(rHands.substring(0,rHands.indexOf("-"))))
+                    && (attributOG.get(i) >= Integer.parseInt(rHands.substring(rHands.indexOf("-")+1,rHands.length())))       )
+                val_String.set(i, rHands);
         }
 
-        if (lHands.size()>4 ) {
-            anonyme(attributOG.subList(0, attributOG.lastIndexOf(lowMediane)));
+       if (lHands.length()>4 ) {
+            anonyme(listeQID.subList(0, listeQID.lastIndexOf(lowMediane)),val_String);
         }
 
-        if (rHands.size()>4) {
-            anonyme(attributOG.subList(attributOG.indexOf(upperMediane), attributOG.size()));
+
+        if (rHands.length()>4) {
+            anonyme(listeQID.subList(listeQID.indexOf(upperMediane), listeQID.size()),val_String);
         }
+
+
 
         return val_String;
     }
